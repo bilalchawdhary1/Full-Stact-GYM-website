faf87cfd26cfc7da2c6a490a5ab9fa71f854e088
Muhammad Bilal <chbilalc627@gmail.com>
2024-10-20 09:32:28 +0500

    c

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Adjust this based on where you're hosting the app
   plugins: [react()],
 })

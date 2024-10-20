535d2801c804bb8c3ee682364f067a471c20a1c5
Muhammad Bilal <chbilalc627@gmail.com>
2024-10-20 10:04:29 +0500

    vite added

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Adjust this based on where you're hosting the app
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Output folder for production build
+    sourcemap: true, // Optional: generate source maps
+  },
+});
